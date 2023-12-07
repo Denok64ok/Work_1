4b04404539ed7f12b173e6307ad191822f158c64
Denis <yaden9483@gmail.com>
2023-12-07 18:15:17 +0500

    Completing the first task

--- PMT/Program.cs
+++ PMT/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+
+        string result = ProcessString(input: inputString);
+
+        Console.WriteLine(result);
+    }
+
+    static string ProcessString(string input)
+    {
+        if (input.Length % 2 == 0)
+        {
+            int halfLength = input.Length / 2;
+
+            string firstHalf = input[..halfLength];
+            string secondHalf = input[halfLength..];
+
+            string reversFirstHalf = ReverseString(firstHalf);
+            string reversSecondHalf = ReverseString(secondHalf);
+
+            return reversFirstHalf + reversSecondHalf;
+        }
+        else
+        {
+            string reversInput = ReverseString(input);
+
+            return reversInput + input;
+        }
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
